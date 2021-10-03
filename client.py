6235f93d41ebe72ecbcb7d407efef57d00916bb0
Vince Coghlan <vincecoghlan@gmail.com>
2021-10-03 12:14:06 -0700

    one last last python2 remnant

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.86.127', 8000))
 msg = 'Hello There, I\'m Python\n'
-s.sendall(msg)
+s.sendall(msg.encode())
 data = s.recv(1024)
 s.close()
 print('Received:\n', data.decode())
